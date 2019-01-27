a26e00b13cee6a535887d0715193e77eb15695d8
Adam Gordon <adamjgordon24@gmail.com>
2019-01-27 23:55:32 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(string)
+  reverse_array = []
+  forward_array = string.split(" ")
+  forward_array.each do |word|
+    reverse_array << word.reverse!
+  end
+  reverse_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.collect do |word|
+    word.reverse!
+  end
+  array.join(" ")
+end
